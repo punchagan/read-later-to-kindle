d6fda65c8bc29a97f864da300129f6c2bb4c01d3
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-08-23 12:03:33 +0530

    email: Add code to send the digest to kindle via email

--- read_later_to_kindle/main.py
+++ read_later_to_kindle/main.py
@@ -1,12 +1,14 @@
 from digest import DigestFactory
 from pinboard import PinboardQueueConsumer
+from send_to_kindle import send_to_kindle
 
 
 def main():
     pinboard_queue = PinboardQueueConsumer()
     factory = DigestFactory()
     entries = pinboard_queue.get_last_unread()
-    factory.create_digest(entries)
+    digest_path = factory.create_digest(entries)
+    send_to_kindle(digest_path)
 
 
 if __name__ == "__main__":
---
+++ read_later_to_kindle/send_to_kindle.py
@@ -0,0 +1,42 @@
+import datetime
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+import os
+import smtplib
+
+DATE = datetime.datetime.now().strftime("%a, %d %b %Y")
+TITLE = "Read Later Digest - {}".format(DATE)
+FROM = os.environ["EMAIL_FROM"]
+TO = os.environ["EMAIL_TO"]
+PASSWORD = os.environ["EMAIL_PASSWORD"]
+HOST = os.environ["EMAIL_HOST"]
+
+
+def send_to_kindle(path):
+    message = _create_message(path)
+    with smtplib.SMTP(host=HOST, port=587) as s:
+        s.starttls()
+        s.login(FROM, PASSWORD)
+        s.send_message(message)
+
+
+def _create_message(path):
+    message = MIMEMultipart()
+    message["Subject"] = "convert"
+    message["From"] = FROM
+    message["To"] = TO
+    message.preamble = TITLE
+
+    # Attach file
+    with open(path, "rb") as fp:
+        attachment = MIMEBase("text", "html")
+        attachment.set_payload(fp.read())
+    encoders.encode_base64(attachment)
+    filename = "{}.html".format(TITLE)
+    attachment.add_header(
+        "Content-Disposition", "attachment", filename=filename
+    )
+    message.attach(attachment)
+
+    return message
