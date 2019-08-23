2ab04780035d9b1d9305b64be543f793c2662770
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-08-23 14:15:00 +0530

    email: Add the digest generation log to the email body

--- read_later_to_kindle/main.py
+++ read_later_to_kindle/main.py
@@ -7,8 +7,8 @@ def main():
     pinboard_queue = PinboardQueueConsumer()
     factory = DigestFactory()
     entries = pinboard_queue.get_last_unread()
-    digest_path, _ = factory.create_digest(entries)
-    send_to_kindle(digest_path)
+    digest_path, log_path = factory.create_digest(entries)
+    send_to_kindle(digest_path, log_path)
 
 
 if __name__ == "__main__":
--- read_later_to_kindle/send_to_kindle.py
+++ read_later_to_kindle/send_to_kindle.py
@@ -1,5 +1,6 @@
 import datetime
 from email import encoders
+from email.message import EmailMessage
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 import os
@@ -13,22 +14,15 @@ PASSWORD = os.environ["EMAIL_PASSWORD"]
 HOST = os.environ["EMAIL_HOST"]
 
 
-def send_to_kindle(path):
-    message = _create_message(path)
+def send_to_kindle(path, log_path):
+    message = _create_message(path, log_path)
     with smtplib.SMTP(host=HOST, port=587) as s:
         s.starttls()
         s.login(FROM, PASSWORD)
         s.send_message(message)
 
 
-def _create_message(path):
-    message = MIMEMultipart()
-    message["Subject"] = "convert"
-    message["From"] = FROM
-    message["To"] = TO
-    message.preamble = TITLE
-
-    # Attach file
+def _create_attachment(path):
     with open(path, "rb") as fp:
         attachment = MIMEBase("text", "html")
         attachment.set_payload(fp.read())
@@ -37,6 +31,22 @@ def _create_message(path):
     attachment.add_header(
         "Content-Disposition", "attachment", filename=filename
     )
+    return attachment
+
+
+def _create_message(path, log_path):
+    with open(log_path) as f:
+        content = "{}\n\n{}".format(TITLE, f.read())
+
+    message = EmailMessage()
+    message["Subject"] = "convert"
+    message["From"] = FROM
+    message["To"] = TO
+    message.preamble = TITLE
+    message.set_content(content)
+    message.make_mixed()
+
+    attachment = _create_attachment(path)
     message.attach(attachment)
 
     return message
